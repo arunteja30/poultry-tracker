19bf1eb1e8858d30d39ee6607eeaeec8f5453c19
Arunteja <arunteja.mididoddy@syncron.com>
2025-09-21 18:45:18 +0530

    Added dockerfile

--- app.py
+++ app.py
@@ -3265,4 +3265,4 @@ def select_company():
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080)
